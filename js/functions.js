b7f13c310ffb2971c9c731163d0145566557b2b8
Eirik Broen <eirikbroen@publishlab.com>
2014-03-13 08:51:46 +0100

    msg

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ function doubleIfMultipleOfFive(num){
 }
 
 function tripleIfMultipleOfThree(num){
-	if(num%3==0) return num*4;
+	if(num%3==0) return num*3;
 }
 
 function quadrupleIfMultipleOfFour(num){
